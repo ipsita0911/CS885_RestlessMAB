6a3061737e475aae9b17863a27711e17330ffbd0
Ipsita Mohanty <45365441+ipsita0911@users.noreply.github.com>
2022-12-04 20:02:26 -0500

    initial commit

---
+++ utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# moving average function
+def numpy_ewma_vectorized_v2(data, window):
+    alpha = 2 /(window + 1.0)
+    alpha_rev = 1-alpha
+    n = data.shape[0]
+
+    pows = alpha_rev**(np.arange(n+1))
+
+    scale_arr = 1/pows[:-1]
+    offset = data[0]*pows[1:]
+    pw0 = alpha*alpha_rev**(n-1)
+
+    mult = data*pw0*scale_arr
+    cumsums = mult.cumsum()
+    out = offset + cumsums*scale_arr[::-1]
+    return out
